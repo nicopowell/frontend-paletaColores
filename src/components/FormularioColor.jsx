b06b7df5f18126ccbde766b82e731914f7b0aa16
Nicolas Powell <nicopowell30@gmail.com>
2023-07-11 17:50:17 -0300

    agregué una prueba en el db.json

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,9 +1,18 @@
 import { Button, Container, Row, Form } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import "./bloqueColor.css";
 import GrillaColores from "./GrillaColores";
+import Swal from "sweetalert2";
 
 const FormularioColor = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+} = useForm();
+
     let coloresLocalStorage = JSON.parse(localStorage.getItem("ListaColores")) || [];
     const [color, setColor] = useState("");
     const [colores, setColores] = useState(coloresLocalStorage);
@@ -12,11 +21,27 @@ const FormularioColor = () => {
         localStorage.setItem("listaColores", JSON.stringify(colores));
     }, [colores]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setColores([...colores, color]);
-        setColor("");
-    };
+    const onSubmit = (tareaNueva) => {
+      consultaAgregarTarea(tareaNueva).then((respuestaCreated) => {
+          if (respuestaCreated && respuestaCreated.status === 201) {
+              Swal.fire(
+                  "Tarea creada",
+                  `La tarea ${tareaNueva.nombreTarea} fue creada correctamente`,
+                  "success"
+              );
+              reset();
+              consultaListaTareas().then((respuesta) => {
+                  setTareas(respuesta);
+              });
+          } else {
+              Swal.fire(
+                  "Ocurrio un error",
+                  `La tarea ${tareaNueva.nombreProducto} no fue creada, intentelo mas tarde`,
+                  "error"
+              );
+          }
+      });
+  };
 
     const borrarColor = (nombreColor) => {
         let copiaColores = colores.filter((itemColor) => itemColor !== nombreColor);
@@ -30,7 +55,7 @@ const FormularioColor = () => {
                     className="rounded border colorDisplay"
                     style={{ backgroundColor: color }}
                 ></div>
-                <Form onSubmit={handleSubmit} className="flex-grow-1 ms-3">
+                <Form onSubmit={handleSubmit(onSubmit)} className="flex-grow-1 ms-3">
                     <Form.Group
                         className="mb-3 d-flex flex-column align-items-end gap-4"
                         controlId="color"
