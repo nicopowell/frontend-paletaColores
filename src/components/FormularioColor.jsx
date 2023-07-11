332f75156a25c832db38111c6eebcd1531651e5a
Nicolas Powell <nicopowell30@gmail.com>
2023-07-11 19:34:48 -0300

    agregué la funcion de crear colores y guardarlos en json server

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -4,17 +4,18 @@ import { useForm } from "react-hook-form";
 import "./bloqueColor.css";
 import GrillaColores from "./GrillaColores";
 import Swal from "sweetalert2";
-import { consultaColores } from "./helpers/queris";
+import { consultaAgregarColor, consultaColores } from "./helpers/queris";
 
 const FormularioColor = () => {
+    const [color, setColor] = useState({});
+    const [colores, setColores] = useState([]);
+
     const {
         register,
         handleSubmit,
         formState: { errors },
         reset,
     } = useForm();
-    const [color, setColor] = useState("");
-    const [colores, setColores] = useState([]);
 
     useEffect(() => {
         consultaColores().then((respuesta) => {
@@ -23,12 +24,33 @@ const FormularioColor = () => {
     }, []);
 
     const onSubmit = (colorNuevo) => {
-        consultaAgregarTarea(colorNuevo).then((respuestaCreated) => {
+        const objetoColor = {};
+        let hexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+        let rgbRegex = /^rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)$/;
+
+        if (hexRegex.test(colorNuevo.color)) {
+            objetoColor.codigoHEX = colorNuevo.color;
+        } else if (rgbRegex.test(colorNuevo.color)) {
+            objetoColor.codigoRGB = colorNuevo.color;
+        } else if (CSS.supports("color", colorNuevo.color.toLowerCase())) {
+            objetoColor.nombreColor = colorNuevo.color;
+        } else {
+            return Swal.fire(
+                          "Ocurrio un error",
+                          `El color ${colorNuevo.color} no es un color valido, intentelo nuevamente`,
+                          "error"
+                      );
+        }
+        consultaAgregarColor(objetoColor).then((respuestaCreated) => {
             if (respuestaCreated && respuestaCreated.status === 201) {
-                Swal.fire("Color creado", `El color fue creado correctamente`, "success");
+                Swal.fire(
+                    "Color creado",
+                    `La tarea ${colorNuevo.color} fue creada correctamente`,
+                    "success"
+                );
                 reset();
-                consultaListaTareas().then((respuesta) => {
-                    setTareas(respuesta);
+                consultaColores().then((respuesta) => {
+                    setColores(respuesta);
                 });
             } else {
                 Swal.fire(
@@ -40,40 +62,41 @@ const FormularioColor = () => {
         });
     };
 
-    const borrarColor = (nombreColor) => {
-        let copiaColores = colores.filter((itemColor) => itemColor !== nombreColor);
-        setColores(copiaColores);
-    };
-
     return (
         <>
             <Container className="d-flex align-items-center mb-5">
                 <div
                     className="rounded border colorDisplay"
-                    style={{ backgroundColor: color }}
+                    style={{ backgroundColor: color.color }}
                 ></div>
                 <Form onSubmit={handleSubmit(onSubmit)} className="flex-grow-1 ms-3">
                     <Form.Group
                         className="mb-3 d-flex flex-column align-items-end gap-4"
                         controlId="color"
                     >
+                        <Form.Text className="text-danger">{errors.nombreTarea?.message}</Form.Text>
                         <Form.Control
                             type="text"
                             placeholder="Ingrese un color"
-                            onChange={(e) => setColor(e.target.value)}
-                            value={color}
                             required
                             maxLength={30}
+                            {...register("color", {
+                                required: "Debe ingresar un color",
+                                maxLength: {
+                                    value: 30,
+                                    message: "La cantidad máxima de caracteres es de 30 dígitos",
+                                },
+                            })}
+                            onChange={(e) => setColor({ color: e.target.value })}
                         />
-                        <Button className="py-3 px-5" variant="primary" type="submit">
+                        <Button className="w-25" variant="primary" type="submit">
                             Guardar
                         </Button>
                     </Form.Group>
                 </Form>
             </Container>
-            <hr className="text-light my-4" />
             <Container>
-                <GrillaColores colores={colores} borrarColor={borrarColor}></GrillaColores>
+                <GrillaColores colores={colores}></GrillaColores>
             </Container>
         </>
     );
